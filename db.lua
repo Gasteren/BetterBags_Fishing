1402b46a5fa57198e42ce1ed10b44601abbea9da
Gasteren <33641309+Gasteren@users.noreply.github.com>
2025-05-27 23:35:47 +0200

    Upload

---
+++ db.lua
@@ -0,0 +1,146 @@
+---@type string, AddonNS
+local _, addon = ...
+
+----------------------------------------- Database -----------------------------------------
+addon.db = {
+	["Fishing"] = {
+		-- Fishing Poles
+		133755, -- Underlight angler
+		44050, -- Mastercraft Kalu'ak Fishing Pole - +30 fishsing and Underwaterbreathing
+		25978, -- Seth's Graphite Fishing Pole - +20 fishsing
+		19022, -- Nat Pagle's Extreme Angler FC-5000 +10 fishing
+		6367, -- Big Iron Fishing Pole - +10 fishing
+		6366, -- Darkwood Fishing Pole - +8 fishing
+		120163, -- Thruk's Fishing Rod - +3 fishing
+		45858, -- Nat's Lucky Fishing Pole - +12 fishing
+		19970, -- Arcanite Fishing Pole - +14 fishing
+		84661, -- Dragon Fishing Pole - +13 fishing
+		45991, -- Bone Fishing Pole
+		118381, -- Ephemeral Fishing Pole - +14 fishing
+		45992, -- Jeweled Fishing Pole - +13 fishing
+		46337, -- Staats' Fishing Pole - +3 fishing
+		12225, -- Blump Family Fishing Pole - +3 fishing | not available anymore
+		6365, -- Strong Fishing Pole - +5 fishing
+		116826, -- Draenic Fishing Pole - +10 fishing AND lure for extra +10
+		84660, -- Pandaren Fishing Pole - +7 fishing
+		116825, -- Savage Fishing Pole - +13 fishing AND lure for extra +10
+		6256, -- Fishing Pole - no skill increase
+		168804, -- Powered Piscine Procurement Pole - +15 fishing
+		173044, -- Wreathed Fishing Pole - +14 fishing
+		180136, -- "The Brokers Angle'r" - +5 fishing
+		88535, -- Sharpened Tuskarr Spear - +5 fishing in area
+		-- Hats
+		117405, -- Nat's Drinking Hat - +8 fishing and +10 from bait.
+		19972, -- Lucky Fishing Hat - +5 fishing and +4 stamina
+		33820, -- Weather-Beaten Fishing Hat
+		93732, -- Darkmoon Fishing Cap - +5 fishing
+		88710, -- Nat's Hat - +5 fishing and +10 fishing from bait
+		118393, -- Tentacled Hat - +5 fishing
+		118380, -- Hightfish Cap - +5 fishing
+		-- Boots
+		19969, -- Nat Pagle's Extreme Anglin' Boots - +5 fishing and +3 stamina
+		50287, -- Boots of the Bay - +10 fishing
+		-- Other
+		68796, -- Reinforced Fishing Line - +1 fishing
+		34836, -- Spun Truesilver Fishing Line - +3 fishing
+		19971, -- High Test Eternium Fishing Line - +5 fishing
+		116117, -- Rook's Lucky Fishin' Line - +4 fishing
+		34832, -- Captain Rumsey's Lager - +5 fishing
+		19979, -- Hook of the Master Angler - Breath underwater and increase swimy
+		67404, -- Glass Fishing Bobber - +2 fishing
+		6529, -- Shiny Bauble - +3 fishing
+		6530, -- Nightcrawlers - +5 fishing
+		6811, -- Aquadynamic Fish Lens - +5 fishing
+		6532, -- Bright Baubles - +7 fishing
+		7307, -- Flesh Eating Worm - +7 fishing
+		6533, -- Aquadynamic Fish Attractor - +9 fishing
+		62673, -- Feathered Lure - +9 fishing
+		34861, -- Sharpened Fish Hook - +9 fishing
+		46006, -- Glow Worm - +9 fishing
+		68049, -- Heat-Treated Spinning Lure - +10 fishing
+		133725, -- 
+		133726, -- 
+		133727, -- 
+		133701, -- 
+		133702, -- 
+		133703, -- 
+		133704, -- 
+		133733, -- 
+		133732, -- 
+		133731, -- 
+		133709, -- 
+		133710, -- 
+		133712, -- 
+		133711, -- 
+		133736, -- 
+		133734, -- 
+		133735, -- 
+		133713, -- 
+		133714, -- 
+		133715, -- 
+		133716, -- 
+		133730, -- 
+		133728, -- 
+		133729, -- 
+		133705, -- 
+		133707, -- 
+		133708, -- 
+		133739, -- 
+		133738, -- 
+		133737, -- 
+		133720, -- 
+		133719, -- 
+		139175, -- 
+		136376, -- 
+		136375, -- 
+		141975, -- 
+		138777, -- 
+		133717, -- 
+		133741, -- 
+		133740, -- 
+		133742, -- 
+		133723, -- 
+		133721, -- 
+		133722, -- 
+		133724, -- 
+		133795, -- 
+		128229, -- 
+		127994, -- 
+		124674, -- 
+		122742, -- 
+		110294, -- 
+		110293, -- 
+		110292, -- 
+		110291, -- 
+		110290, -- 
+		110289, -- 
+		110274, -- 
+		146848, -- 
+		136377, -- 
+		173038, -- 
+		173040, -- 
+		173041, -- 
+		173039, -- 
+		173042, -- 
+		173043, -- 
+		180168, -- 
+		194725, --
+		----------------------- War within -----------------------------
+		224752, -- Soaked Journal Entry (increase fishing skill by 10 stacks till 300)
+		221790, -- Fishing pole, Bismuth
+		221791, -- Fishing pole, agirite
+		224085, -- temp fish cap - hightfish cap
+		224084, -- temp fish cap - tentacled hat
+		222842, -- fishing hat
+		225771, -- Seekerthread for fish increase
+		225770, -- Anglerthread for fish increase
+		225729, -- Weaverline - completed all threads
+		220138, --
+		220152, --
+		220134, --
+		220136, --
+		220135, --
+		220137, --
+		228954, -- Anglers guide to learn "find fish"
+	},
+}
